e7a9c3564e42f129db9f94bf7544c9842fd5ac02
ahmed osman <ahmedos099912@gmail.com>
2023-12-17 15:07:01 +0200

    fixing sizing

--- app/components/productlist.jsx
+++ app/components/productlist.jsx
@@ -14,7 +14,7 @@ export default function Productlist() {
             <Image
             src={img1}
             alt='ad' quality={100} placeholder='blur'
-            className=' mx-auto'
+            className=' mx-auto w-9/12'
             >
             </Image>
             <div className='p-1 rounded-sm bg-red-500 text-white absolute top-0 text-sm'>
@@ -47,7 +47,7 @@ export default function Productlist() {
             <Image
             src={img1}
             alt='ad' quality={100} placeholder='blur'
-            className=' mx-auto'
+            className=' mx-auto w-9/12'
             >
             </Image>
             <div className='p-1 rounded-sm bg-red-500 text-white absolute top-0 text-sm'>
@@ -80,7 +80,7 @@ export default function Productlist() {
             <Image
             src={img1}
             alt='ad' quality={100} placeholder='blur'
-            className=' mx-auto'
+            className=' mx-auto w-9/12'
             >
             </Image>
             <div className='p-1 rounded-sm bg-red-500 text-white absolute top-0 text-sm'>
@@ -113,7 +113,7 @@ export default function Productlist() {
             <Image
             src={img1}
             alt='ad' quality={100} placeholder='blur'
-            className=' mx-auto'
+            className=' mx-auto w-9/12'
             >
             </Image>
             <div className='p-1 rounded-sm bg-red-500 text-white absolute top-0 text-sm'>
@@ -140,6 +140,40 @@ export default function Productlist() {
         <p className='text-gray-500 ml-2'>(88)</p>
         </div>
     </div>
+    <div className=''>
+        <div className="container w-56 bg-gray-100 p-1 rounded-sm">
+            <div className='relative'>
+            <Image
+            src={img1}
+            alt='ad' quality={100} placeholder='blur'
+            className=' mx-auto w-9/12'
+            >
+            </Image>
+            <div className='p-1 rounded-sm bg-red-500 text-white absolute top-0 text-sm'>
+                -47%
+            </div>
+            <MdFavoriteBorder className=' absolute top-0 right-0 text-2xl'/>
+            <MdOutlinePreview className=' absolute top-10 right-0 text-2xl'/>
+            </div>
+            
+            
+        </div>
+        <h4>Playstation 4 controller</h4>
+        <div className='flex'>
+        <p className='text-red-500'>$120</p>
+        <p className='text-gray-500 line-through ml-2'>$120</p>
+        </div>
+
+        <div className='flex text-yellow-500 items-center'>
+        <FaStar />
+        <FaStar />
+        <FaStar />
+        <FaStar />
+        <FaStar />
+        <p className='text-gray-500 ml-2'>(88)</p>
+        </div>
+    </div>
+   
     </div>
   )
 }
