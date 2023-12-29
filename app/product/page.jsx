bdd1a02ea8baf87f7b410cf36496ce26694fa385
ahmed osman <ahmedos099912@gmail.com>
2023-12-29 12:36:53 +0200

    product page

--- app/product/page.jsx
+++ app/product/page.jsx
@@ -18,6 +18,21 @@ export default function Product() {
         if(counter>0)
         setCounter(counter-1)
     }
+    
+    const changeSize = event => {
+        for(var child of event.target.parentNode.parentNode.children){
+           child.className = 'ml-2  border-2 border-gray-200 rounded-full p-1'
+        }
+        event.target.parentNode.className += ' border-gray-500'
+      }
+
+      const changeColor = event => {
+        for(var child of event.target.parentNode.parentNode.children){
+           child.className = 'ml-2  border-2 border-gray-200 rounded-full p-1'
+        }
+        event.target.parentNode.className += ' border-gray-500'
+      }
+
   return (
     <div className='flex flex-row'>
         <div className='basis-5/12 h-4/6'>
@@ -62,44 +77,44 @@ export default function Product() {
                 <p className='text-lg font-bold mb-4'>Choose color</p>
                 <div className="flex items-center">
                 <div className='ml-2  border-2 border-gray-200 rounded-full p-1'>
-                    <div className='w-5 h-5 bg-red-500 rounded-full'></div>
+                    <div className='w-5 h-5 bg-red-500 rounded-full' onClick={changeColor}></div>
                 </div>
                 <div className='ml-2  border-2 border-gray-500 rounded-full p-1'>
-                    <div className='w-5 h-5 bg-blue-400 rounded-full'></div>
+                    <div className='w-5 h-5 bg-blue-400 rounded-full' onClick={changeColor}></div>
                 </div>
                 <div className='ml-2  border-2 border-gray-200 rounded-full p-1'>
-                    <div className='w-5 h-5 bg-black rounded-full'></div>
+                    <div className='w-5 h-5 bg-black rounded-full' onClick={changeColor}></div>
                 </div>
                 <div className='ml-2  border-2 border-gray-200 rounded-full p-1'>
-                    <div className='w-5 h-5 bg-green-500 rounded-full'></div>
+                    <div className='w-5 h-5 bg-green-500 rounded-full' onClick={changeColor}></div>
                 </div>
                 </div>
                 </div>
 
 
                 <div>
-                <p className='text-lg font-bold mb-4'>Avaiable Sizes</p>
+                <p className='text-lg font-bold mb-4' id='sizes'>Avaiable Sizes</p>
                 <div className="flex items-center">
                 <div className='ml-2  border-2 border-gray-200 rounded-full p-1'>
-                    <div className=''>38</div>
+                    <div className='cursor-pointer' onClick={changeSize}>38</div>
                 </div>
-                <div className='ml-2  border-2 border-gray-500 rounded-full p-1'>
-                <div className=''>39</div>
-                </div>
-                <div className='ml-2  border-2 border-gray-200 rounded-full p-1'>
-                <div className=''>40</div>
+                <div className='ml-2  border-2 border-gray-500 rounded-full p-1' >
+                <div className='cursor-pointer' onClick={changeSize}>39</div>
                 </div>
                 <div className='ml-2  border-2 border-gray-200 rounded-full p-1'>
-                <div className=''>41</div>
+                <div className='cursor-pointer' onClick={changeSize}>40</div>
                 </div>
                 <div className='ml-2  border-2 border-gray-200 rounded-full p-1'>
-                <div className=''>42</div>
+                <div className='cursor-pointer' onClick={changeSize}>41</div>
                 </div>
                 <div className='ml-2  border-2 border-gray-200 rounded-full p-1'>
-                <div className=''>43</div>
+                <div className='cursor-pointer' onClick={changeSize}>42</div>
                 </div>
                 <div className='ml-2  border-2 border-gray-200 rounded-full p-1'>
-                <div className=''>44</div>
+                <div className='cursor-pointer' onClick={changeSize}>43</div>
+                </div>
+                <div className='ml-2  border-2 border-gray-200 rounded-full p-1'>
+                <div className='cursor-pointer' onClick={changeSize}>44</div>
                 </div>
                 </div>
                 </div>
@@ -110,13 +125,13 @@ export default function Product() {
             <div className='flex items-center justify-between mx-10 mt-8'>
                 <div className='flex items-center'>
                 <p>QTY</p>
-                <button className='w-10 h-10 border p-2 ml-4 rounded-s' onClick={decrease}>-</button>
+                <button className='w-10 h-10 border p-2 ml-4 rounded-s hover:bg-slate-100' onClick={decrease}>-</button>
                 <div className='w-10 h-10 border p-2 text-center'>{counter}</div>
-                <button className='w-10 h-10 border p-2 rounded-e' onClick={increase}>+</button>
+                <button className='w-10 h-10 border p-2 rounded-e hover:bg-slate-100' onClick={increase}>+</button>
                 </div>
                 <div className='flex'>
-                    <button className=' bg-red-500 py-2 px-12 h-10 text-white rounded mr-4'>Buy now</button>
-                    <button className=' border-red-500 border-2 py-2 px-12 h-10 rounded text-red-500'>Add to Cart</button>
+                    <button className=' bg-red-500 py-2 px-12 h-10 text-white rounded mr-4 hover:bg-red-600'>Buy now</button>
+                    <button className=' border-red-500 border-2 py-2 px-12 h-10 rounded text-red-500 hover:bg-gray-100'>Add to Cart</button>
                 </div>
             </div>
             <div className=' border bg-gray-800 mx-10 mt-8'></div>
