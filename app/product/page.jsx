e544de0de180a70b28536523212c6606949bcd7d
ahmed osman <ahmedos099912@gmail.com>
2024-01-14 15:40:07 +0200

    alignment

--- app/product/page.jsx
+++ app/product/page.jsx
@@ -149,8 +149,6 @@ export default function Product() {
             </div>
             <div className=' border bg-gray-800 mx-10 mt-8'></div>
 
-
-
             <div className='mt-8 mx-8'>
             <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
                 <li className="me-2">
