5e5e334e228d9825d9693014314d1873a00fe313
ahmed osman <ahmedos099912@gmail.com>
2024-01-26 03:22:21 +0400

    style:hover side menu

--- app/page.js
+++ app/page.js
@@ -4,23 +4,24 @@ import Header from './components/header'
 import Productlist from './components/productlist'
 import Categories from './components/categories'
 import Footer from './components/footer'
+import { HiOutlineArrowSmRight } from "react-icons/hi";
 
 
 export default function Home() {
   return (
     <main>
       <div className="grid grid-cols-12 gap-4 items-center">
-        <div className='border-e-2 w-full flex flex-col items-center col-span-3'>
-        <ul className='mt-10 text-lg'>
-          <li>Women's fashion</li>
-          <li>Men's fashion</li>
-          <li>Elactronics</li>
-          <li>Home & lifestyle</li>
-          <li>Medicine</li>
-          <li>Sports & Outdoor</li>
-          <li>Baby's & Toy's</li>
-          <li>Groceries & Pets</li>
-          <li>Health & Beauty</li>
+        <div className='border-e-2 w-full flex flex-col col-span-3'>
+        <ul className='mt-10 text-lg mx-auto p-2'>
+          <li className='flex items-center group '><HiOutlineArrowSmRight className='invisible  group-hover:visible '/><p className='group-hover:ml-2'></p>Women's fashion</li>
+          <li className='flex items-center group '><HiOutlineArrowSmRight className='invisible  group-hover:visible '/><p className='group-hover:ml-2'>Men's fashion</p></li>
+          <li className='flex items-center group '><HiOutlineArrowSmRight className='invisible  group-hover:visible '/><p className='group-hover:ml-2'>Elactronics</p></li>
+          <li className='flex items-center group '><HiOutlineArrowSmRight className='invisible  group-hover:visible '/><p className='group-hover:ml-2'>Home & lifestyle</p></li>
+          <li className='flex items-center group '><HiOutlineArrowSmRight className='invisible  group-hover:visible '/><p className='group-hover:ml-2'>Medicine</p></li>
+          <li className='flex items-center group '><HiOutlineArrowSmRight className='invisible  group-hover:visible '/><p className='group-hover:ml-2'>Sports & Outdoor</p></li>
+          <li className='flex items-center group '><HiOutlineArrowSmRight className='invisible  group-hover:visible '/><p className='group-hover:ml-2'>Baby's & Toy's</p></li>
+          <li className='flex items-center group '><HiOutlineArrowSmRight className='invisible  group-hover:visible '/><p className='group-hover:ml-2'>Groceries & Pets</p></li>
+          <li className='flex items-center group '><HiOutlineArrowSmRight className='invisible  group-hover:visible '/><p className='group-hover:ml-2'>Health & Beauty</p></li>
         </ul>
         </div>
         <div className="col-span-8 mt-10">
